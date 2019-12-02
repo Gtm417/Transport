74493fb57aa1d49f20f34882e6e6a6ec163b9bb4
Gtm417 <47758495+Gtm417@users.noreply.github.com>
2019-12-02 22:53:25 +0200

    Реализация метода compareTo для Вагонов и сортировка Вагонов поезда

--- src/com/company/model/entity/ComfortLevel.java
+++
@@ -1,7 +0,0 @@
-package com.company.model.entity;
-
-public enum ComfortLevel {
-    WEAK, NORMAL, HIGH, VIP
-
-
-}
--- src/com/company/view/ConsoleView.java
+++
@@ -1,8 +0,0 @@
-package com.company.view;
-
-public class ConsoleView {
-
-    public void printMessage(String message){
-       System.out.println(message);
-    }
-}
