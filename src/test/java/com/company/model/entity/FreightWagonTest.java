bfe552f851a8b0e6a70954bc47dc053ae93475df
Gtm417 <47758495+Gtm417@users.noreply.github.com>
2019-12-03 15:03:30 +0200

    Tests compareTo FreightWagons (Bigger to Less, Less to Bigger and Equals)

--- src/test/java/com/company/model/entity/FreightWagonTest.java
+++ src/test/java/com/company/model/entity/FreightWagonTest.java
@@ -17,4 +17,31 @@ public class FreightWagonTest {
         int result = freightWagon.compareTo(new PassengerWagon("name2", 312,ComfortLevel.NORMAL));
         assertEquals(1, result);
     }
+
+    @Test
+    public void testCompareToFreightWagonsByMaxCarryingCapacityBiggerToLow(){
+        int result = freightWagon.compareTo(new FreightWagon("name3", 3123,9,27));
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void testCompareToFreightWagonsByMaxCarryingCapacityLesserToBigger(){
+        int result =(new FreightWagon("name3", 3123,9,27)).compareTo(freightWagon);
+        assertEquals(-1,result);
+    }
+
+    @Test
+    public void testCompareToEqualsFreightWagonsByRef(){
+        FreightWagon newWagon = freightWagon;
+        int result = freightWagon.compareTo(newWagon);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testCompareToEqualsFreightWagonsByMaxCarryingCapacity(){
+        FreightWagon newWagon = new FreightWagon("name44", 312, 3,54);
+        int result = freightWagon.compareTo(newWagon);
+        assertEquals(0, result);
+    }
+
 }
