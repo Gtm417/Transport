94df80dfec1897787c9bb01050dbd40e314832a7
Gtm417 <47758495+Gtm417@users.noreply.github.com>
2019-12-04 18:46:47 +0200

    added ResourceBundle "messages",  ViewConstants, Finish class ConsoleView

--- src/main/java/com/company/view/ConsoleView.java
+++ src/main/java/com/company/view/ConsoleView.java
@@ -1,8 +1,29 @@
 package com.company.view;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ConsoleView {
 
     public void printMessage(String message){
        System.out.println(message);
+    }
+
+
+    // Resource Bundle Installation's
+    static String MESSAGES_BUNDLE_NAME = "messages";
+    public static final ResourceBundle bundle =
+            ResourceBundle.getBundle(
+                    MESSAGES_BUNDLE_NAME,
+                    //new Locale("ukr", "UKR"));  // Ukrainian
+                    new Locale("en"));        // English
+
+
+    public String concatenationString(String... message){
+        StringBuilder concatString = new StringBuilder();
+        for(String v : message) {
+            concatString.append(v);
+        }
+        return new String(concatString);
     }
 }
---
+++ src/main/java/com/company/view/ViewConstants.java
@@ -0,0 +1,9 @@
+package com.company.view;
+
+public interface ViewConstants {
+    String PRINT_TRAIN_DATA = "print.train.data";
+    String PRINT_TRAIN_WAGONS = "print.train.wagons";
+    String PRINT_SORTED_TRAIN_WAGONS = "print.sorted.train.wagons";
+    String PRINT_TRAIN_WAGONS_IN_DIAPASON = "print.train.wagons.in.diapason";
+
+}
